9da0053e895dba2e9cc23fcd4edf3472a8902a8e
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-05-14 00:14:06 -0400

    *sigh*

--- app/mc_client.py
+++ app/mc_client.py
@@ -26,11 +26,11 @@ class McClient:
         self.outq_read_thread = threading.Thread(target=self._out_reader)
         self.outq_read_thread.start()
         self.proc = subprocess.Popen(
-            ["./bedrock_server"],
+            ["cd ~/circuitcraft && ./bedrock_server"],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            cwd="~/circuitcraft"
+            shell=True,
         )
 
     def close(self):
